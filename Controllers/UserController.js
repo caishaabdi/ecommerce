79c9748b30d939a18282a8b80b989867db7652de
caishaabdi <caishaabdlla@gmail.com>
2023-12-06 10:17:09 +0300

    frst comit

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -21,14 +21,16 @@ export const register = async(req, res) => {
             })
 
             if (createuser) {
-                res.status(201).json
-                _id: createuser._id;
-                name: createuser.name
-                email: createuser.email
-                password: createuser.password
-                address: createuser.address
-                phone: createuser.phone
-                token: generateToken(createuser._id)
+                res.status(201).json({
+                    _id: createuser._id,
+                    name: createuser.name,
+                    email: createuser.email,
+                    password: createuser.password,
+                    address: createuser.address,
+                    phone: createuser.phone,
+                    token: generateToken(createuser._id)
+                })
+
             } else {
                 res.status(401).json({ message: 'Invalid User Data' });
             }
@@ -46,23 +48,41 @@ export const login = async(req, res) => {
 
     // try {
     const { email, password } = req.body;
-    const userLOgin = await UserModel.findOne({ email })
-    if (userLOgin && password == userLOgin.password) {
-        res.status(200).json
-        _id: createuser._id;
-        name: createuser.name
-        email: createuser.email
-        password: createuser.password
-        address: createuser.address
-        phone: createuser.phone
-        token: generateToken(userLOgin._id)
+    const user = await UserModel.findOne({ email })
+    if (user && password == user.password) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            password: user.password,
+            address: user.address,
+            phone: user.phone,
+            token: generateToken(user._id)
+        })
 
     } else {
         res.status(404).json({ message: 'Invalid gmail or password' });
 
     }
-    // } catch (error) {
-    //     res.status(500).json({
-    //         error: error.message
-    //     });
+
+}
+
+export const getUserProfile = async(req, res) => {
+    const { id, token } = req.body;
+    const user = await UserModel.findOne({ id })
+    if (user) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            password: user.password,
+            address: user.address,
+            phone: user.phone,
+            token
+        })
+
+    } else {
+        res.status(404).json({ message: 'Invalid user data' });
+
+    }
 }
