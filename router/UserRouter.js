12f61fb33b1af0569aa025dc45f382a73181814b
caishaabdi <caishaabdlla@gmail.com>
2023-12-11 19:00:06 +0300

    second commit

--- router/UserRouter.js
+++ router/UserRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { login, register } from "../Controllers/UserController.js";
+import { getUserProfile, login, register } from "../Controllers/UserController.js";
 const router = express.Router()
 
 router.route('/').post(register)
+router.route('/profile').post(getUserProfile)
 router.route('/login').post(login)
 
 export default router
