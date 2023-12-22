c714ab4e8c58160aa15107eddc640c6a3e0cdb50
caishaabdi <caishaabdlla@gmail.com>
2023-12-22 21:49:26 +0300

    second commit

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -3,101 +3,101 @@ import generateToken from "../Utilies/GenarateToken.js"
 
 export const register = async(req, res) => {
 
-    try {
-        const { name, email, password, address, phone } = req.body
-        const userExisit = await UserModel.findOne({ email })
+    // try {
+    const { name, email, password, address, phone } = req.body
+    const userExisit = await UserModel.findOne({ email })
 
-        if (userExisit) {
-            res.status(400).json({
-                message: 'user alredy exists'
+    if (userExisit) {
+        res.status(400).json({
+            message: 'user alredy exists'
+        })
+    } else {
+        createuser = UserModel.create({
+            name,
+            email,
+            password,
+            address,
+            phone,
+        })
+
+        if (createuser) {
+            res.status(201).json({
+                _id: createuser._id,
+                name: createuser.name,
+                email: createuser.email,
+                password: createuser.password,
+                address: createuser.address,
+                phone: createuser.phone,
+                token: generateToken(createuser._id)
             })
+
         } else {
-            createuser = UserModel.create({
-                name,
-                email,
-                password,
-                address,
-                phone,
-            })
-
-            if (createuser) {
-                res.status(201).json({
-                    _id: createuser._id,
-                    name: createuser.name,
-                    email: createuser.email,
-                    password: createuser.password,
-                    address: createuser.address,
-                    phone: createuser.phone,
-                    token: generateToken(createuser._id)
-                })
-
-            } else {
-                res.status(401).json({ message: 'Invalid User Data' });
-            }
-
-
+            res.status(401).json({ message: 'Invalid User Data' });
         }
-    } catch (error) {
-        // print(e.toString());
-        res.status(500).json({
-            error: error.message
-        });
-    }
 
-};
+
+    }
+    // } catch (error) {
+    //     // print(e.toString());
+    //     res.status(500).json({
+    //         error: error.message
+    //     });
+}
+
+
 
 
 export const login = async(req, res) => {
 
-    try {
-        const { email, password } = req.body;
-        const user = await UserModel.findOne({ email })
-        if (user && password == user.password) {
-            res.status(200).json({
-                _id: user._id,
-                name: user.name,
-                email: user.email,
-                password: user.password,
-                address: user.address,
-                phone: user.phone,
-                isAdmin: user.isAdmin,
-                token: generateToken(user._id)
-            })
-
-        } else {
-            res.status(404).json({ message: 'Invalid gmail or password' });
-
-        }
-
-    } catch (error) {
-        res.status(500).json({
-            error: error.message
+    // try {
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({ email })
+    if (user && password == user.password) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            password: user.password,
+            address: user.address,
+            phone: user.phone,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
         })
+
+    } else {
+        res.status(404).json({ message: 'Invalid gmail or password' });
+
     }
+
+    // } catch (error) {
+    //     res.status(500).json({
+    //         error: error.message
+    //     })
+    // }
 }
 export const getUserProfile = async(req, res) => {
-    try {
-        const { id, token } = req.body;
-        const user = await UserModel.findOne({ id })
-        if (user) {
-            res.status(200).json({
-                _id: user._id,
-                name: user.name,
-                email: user.email,
-                password: user.password,
-                address: user.address,
-                phone: user.phone,
-                isAdmin: user.isAdmin,
-                token
-            })
-
-        } else {
-            res.status(404).json({ message: 'Invalid user data' });
-
-        }
-    } catch (error) {
-        res.status(500).json({
-            error: error.message
+    // try {
+    const { id, token } = req.body;
+    const user = await UserModel.findOne({ id })
+    if (user) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            password: user.password,
+            address: user.address,
+            phone: user.phone,
+            isAdmin: user.isAdmin,
+            token
         })
+
+    } else {
+        res.status(404).json({ message: 'Invalid user data' });
+
     }
+    // } catch (error) {
+    //     res.status(500).json({
+    //         error: error.message
+    //     })
+    // }
 }
