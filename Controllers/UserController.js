ff18cc9f47abf6a94dbffc10e9fae6d0e143a5fd
caishaabdi <caishaabdlla@gmail.com>
2023-12-22 21:32:04 +0300

    second commit

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -37,8 +37,11 @@ export const register = async(req, res) => {
 
 
         }
-    } catch (e) {
-        print(e.toString());
+    } catch (error) {
+        // print(e.toString());
+        res.status(500).json({
+            error: error.message
+        });
     }
 
 };
@@ -46,45 +49,55 @@ export const register = async(req, res) => {
 
 export const login = async(req, res) => {
 
-    // try {
-    const { email, password } = req.body;
-    const user = await UserModel.findOne({ email })
-    if (user && password == user.password) {
-        res.status(200).json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            password: user.password,
-            address: user.address,
-            phone: user.phone,
-            isAdmin: user.isAdmin,
-            token: generateToken(user._id)
+    try {
+        const { email, password } = req.body;
+        const user = await UserModel.findOne({ email })
+        if (user && password == user.password) {
+            res.status(200).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                password: user.password,
+                address: user.address,
+                phone: user.phone,
+                isAdmin: user.isAdmin,
+                token: generateToken(user._id)
+            })
+
+        } else {
+            res.status(404).json({ message: 'Invalid gmail or password' });
+
+        }
+
+    } catch (error) {
+        res.status(500).json({
+            error: error.message
         })
-
-    } else {
-        res.status(404).json({ message: 'Invalid gmail or password' });
-
     }
-
 }
-
 export const getUserProfile = async(req, res) => {
-    const { id, token } = req.body;
-    const user = await UserModel.findOne({ id })
-    if (user) {
-        res.status(200).json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            password: user.password,
-            address: user.address,
-            phone: user.phone,
-            isAdmin: user.isAdmin,
-            token
+    try {
+        const { id, token } = req.body;
+        const user = await UserModel.findOne({ id })
+        if (user) {
+            res.status(200).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                password: user.password,
+                address: user.address,
+                phone: user.phone,
+                isAdmin: user.isAdmin,
+                token
+            })
+
+        } else {
+            res.status(404).json({ message: 'Invalid user data' });
+
+        }
+    } catch (error) {
+        res.status(500).json({
+            error: error.message
         })
-
-    } else {
-        res.status(404).json({ message: 'Invalid user data' });
-
     }
 }
