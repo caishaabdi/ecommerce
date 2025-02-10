a6c425316feb754a7afbc0fab18ffd1080c7ee27
aisha <caishaabdlla@gmail.com>
2025-02-10 15:11:26 +0300

    ecoooo

--- Database/db.js
+++ Database/db.js
@@ -1,30 +1,29 @@
-import mongoose from 'mongoose'
+// import mongoose from 'mongoose'
 
-const ConnectDBi = () => {
-    mongoose.connect(process.env.MANGO_URL, {
+// const ConnectDBi = () => {
+//     mongoose.connect(process.env.MANGO_URL, {
 
-    })
-        .then(() => {
-            console.log("Database connected")
-        });
-}
-export default ConnectDBi
-
-
-
-// import mongoose from 'mongoose';
-
-// const ConnectDBi = async () => {
-//     try {
-//         await mongoose.connect(process.env.MANGO_URL, {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
+//     })
+//         .then(() => {
+//             console.log("Database connected")
 //         });
-//         console.log('MongoDB connected');
-//     } catch (error) {
-//         console.error(error);
-//         process.exit(1); // Exit process with failure
-//     }
-// };
+// }
+// export default ConnectDBi
 
-// export default ConnectDBi;
+
+
+import mongoose from 'mongoose';
+
+const ConnectDBi = async () => {
+    try {
+        await mongoose.connect(process.env.MANGO_URL, {
+
+        });
+        console.log('MongoDB connected');
+    } catch (error) {
+        console.error(error);
+        process.exit(1); // Exit process with failure
+    }
+};
+
+export default ConnectDBi;
